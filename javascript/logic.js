6342b8066cfc11d5b59d7458c5cc35214e312543
Your Name <s.nguyen11@utexas.edu>
2017-03-29 11:34:18 -0500

    creating new row every time submit button is pressed

---
+++ javascript/logic.js
@@ -0,0 +1,25 @@
+  var name = "";
+  var role = "";
+  var startDate = "";
+  var rate = 0;
+  var monthsWorked = 0;
+  var totalBilled = 0;
+
+  $("#submit").on("click", function(event){
+  	event.preventDefault();
+  	var row = [
+  		name = $("#employeeName").val().trim(),
+  		role = $("#role").val().trim(),
+  		startDate = $("#startDate").val().trim(),
+  		monthsWorked = " ",
+  		rate = $("#monthlyRate").val().trim(),
+  		totalBilled =  " "
+  	]
+
+  	var newRow = $("<tr>");
+    for(i=0;i<6;i++){
+      	var data = $("<td>").text(row[i]);
+      	newRow.append(data);
+    }; 
+    $("#rows").prepend(newRow);
+  })
