d8c245755f101274477f6089ccd3fc313a77bcaa
Anastasia Marie <ana.goodwin10@gmail.com>
2017-03-30 10:14:38 -0500

    updated javascript and html

--- javascript/logic.js
+++ javascript/logic.js
@@ -21,17 +21,17 @@ var currentDate = moment().format('MM/DD/YY');
 $("#submit").on("click", function(event){
   event.preventDefault();
   // adding user input into an array called row
-  var row = [
-    name = $("#employeeName").val().trim(),
-    role = $("#role").val().trim(),
+  
+    name = $("#employeeName").val().trim();
+    role = $("#role").val().trim();
     // using moment to grab start date user submitted in order to be able to calculate months worked
-    start = moment($("#startDate").val().trim()).format('MM/DD/YY'),
+    start = moment($("#startDate").val().trim(), "DD/MM/YY").format("X");
+    console.log("start" + start);
     //using .diff to subtract start date from current date, we want the result in months, turn result to an integer
     
-    rate = parseInt($("#monthlyRate").val().trim()),
-   
-  ]
-   
+    rate = parseInt($("#monthlyRate").val().trim());
+  
+   console.log(start)
 
     database.ref().push({
         name: name,
@@ -44,17 +44,16 @@ $("#submit").on("click", function(event){
 
 
 database.ref().on("child_added", function(Snapshot) {
-  // var totalBilled="";
-  // var monthsWorked=0;
+    var start= Snapshot.val().start;
   
   // function(){
-    monthsWorked= parseInt(moment().diff(moment(Snapshot.val().start), 'months'));
+    monthsWorked= moment().diff(moment.unix(start, "X"), "months");
     totalBilled= monthsWorked* Snapshot.val().rate;
-    console.log(monthsWorked)
-    console.log(totalBilled);
+    
+    var newDate=moment.unix(start).format("MM/DD/YY");
 
   
 
 
-$("#rows").append("<tr><td>" + Snapshot.val().name + "</td><td>" + Snapshot.val().role + "</td><td>" +  Snapshot.val().start + "</td><td>" + monthsWorked  +"</td><td>" +  Snapshot.val().rate + "</td><td>" + totalBilled  +"</td></tr>");
+$("#rows").append("<tr><td>" + Snapshot.val().name + "</td><td>" + Snapshot.val().role + "</td><td>" +  newDate + "</td><td>" + monthsWorked  +"</td><td>" +  Snapshot.val().rate + "</td><td>" + totalBilled  +"</td></tr>");
 });
